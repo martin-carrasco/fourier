2cf7b05325b9dd63f9d155eaf5ea307b88384010
dgcnz <canezdiego@gmail.com>
2019-06-19 21:51:00 -0500

    Added inverse fft

--- code/lib/fft.cpp
+++ code/lib/fft.cpp
@@ -37,3 +37,37 @@ vector<cn> recursive_fft(const vector<cn>& a)
     }
     return y;
 }
+
+vector<cn> recursive_ifft(const vector<cn>& y)
+{
+    int n;
+    double rad;
+
+    n = y.size();
+    if (not(n && !(n & (n - 1))))
+        throw std::runtime_error("Not a power of 2\n");
+    if (n == 1)
+        return { cn((y[0].real() / n), (y[0].imag() / 2)) };
+
+    vector<cn> y0(n / 2), y1(n / 2);
+    vector<cn> a0(n / 2), a1(n / 2);
+    vector<cn> a(n);
+
+    for (int i = 0; i < n / 2; ++i) {
+        y0[i] = y[2 * i];
+        y1[i] = y[2 * i + 1];
+    }
+
+    a0 = recursive_fft(y0);
+    a1 = recursive_fft(y1);
+
+    rad = 2 * M_PI / n;
+    cn w(1 / n), wn(cos(rad), sin(rad));
+
+    for (int k = 0; k <= n / 2 - 1; ++k) {
+        a[k] = a0[k] + w * a1[k];
+        a[k + (n / 2)] = a0[k] - w * a1[k];
+        w = w * wn;
+    }
+    return a;
+}
