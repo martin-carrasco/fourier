886cdac5123b9ed483cd1307fbcfef7ea85278d4
dgcnz <canezdiego@gmail.com>
2019-06-20 16:41:39 -0500

    Fixed ifft

--- code/lib/fft.cpp
+++ code/lib/fft.cpp
@@ -10,18 +10,15 @@ vector<cn> _recursive_fft(const vector<cn>& a, bool inverse)
     double rad;
 
     n = a.size();
+    vector<cn> y(n);
+
     if (not(n && !(n & (n - 1))))
         throw std::runtime_error("Not a power of 2\n");
-    if (n == 1) {
-        if (not inverse)
-            return a;
-        else
-            return { cn((a[0].real() / n), (a[0].imag() / 2)) };
-    }
+    if (n == 1)
+        return (y = a);
 
     vector<cn> a0(n / 2), a1(n / 2);
     vector<cn> y0(n / 2), y1(n / 2);
-    vector<cn> y(n);
 
     for (int i = 0; i < n / 2; ++i) {
         a0[i] = a[2 * i];
@@ -31,12 +28,16 @@ vector<cn> _recursive_fft(const vector<cn>& a, bool inverse)
     y0 = _recursive_fft(a0, inverse);
     y1 = _recursive_fft(a1, inverse);
 
-    rad = 2 * M_PI / n;
-    cn w(inverse ? 1 : 1 / n), wn(cos(rad), sin(rad));
+    rad = 2 * M_PI / n * (inverse ? -1 : 1);
+    cn w(1), wn(cos(rad), sin(rad));
 
     for (int k = 0; k <= n / 2 - 1; ++k) {
         y[k] = y0[k] + w * y1[k];
         y[k + (n / 2)] = y0[k] - w * y1[k];
+        if (inverse) {
+            y[k] /= 2;
+            y[k + n / 2] /= 2;
+        }
         w = w * wn;
     }
     return y;
--- code/src/main.cpp
+++ code/src/main.cpp
@@ -8,8 +8,22 @@ int main(void)
 {
     vector<cn> x = { 0.24200326, 0.96688159, 0.87782714, 0.8408777 };
     vector<cn> y = recursive_fft(x);
+    vector<cn> xp = recursive_ifft(y);
 
+    cout << "Originally in time domain:\n";
+    for (auto i : x) {
+        cout << i << " ";
+    }
+    cout << endl;
+
+    cout << "In frequency domain:\n";
     for (auto i : y) {
+        cout << i << " ";
+    }
+    cout << endl;
+
+    cout << "Again in time domain:\n";
+    for (auto i : xp) {
         cout << i << " ";
     }
     cout << endl;
