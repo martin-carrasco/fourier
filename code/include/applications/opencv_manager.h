239d10d2f54f3aa8f9dd8ede785e02fcc289b3bd
dgcnz <canezdiego@gmail.com>
2019-06-21 14:40:06 -0500

    New folder structure

---
+++ code/include/applications/opencv_manager.h
@@ -0,0 +1,24 @@
+#ifndef __OPENCV_MANAGER_H__
+#define __OPENCV_MANAGER_H__
+
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+/*
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/imgproc.hpp>
+
+#define FILENAME "../input/lenna.png"
+#define OUTPUT_FILE "../output/output.txt"
+#define INPUT_FILE "../input/input.txt"
+
+typedef cv::Mat Img;
+
+void generate_csv();
+void display_csv();
+*/
+#endif
---
+++ code/test/CMakeFiles/projectname-unittests.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/dgcnz/development/utec/ada/fourier/code/test/main.cpp" "/Users/dgcnz/development/utec/ada/fourier/code/test/CMakeFiles/projectname-unittests.dir/main.cpp.o"
+  "/Users/dgcnz/development/utec/ada/fourier/code/test/sanity_check.cpp" "/Users/dgcnz/development/utec/ada/fourier/code/test/CMakeFiles/projectname-unittests.dir/sanity_check.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "thirdparty/include"
+  "include"
+  "/usr/local/Cellar/portaudio/19.6.0/include"
+  "/usr/local/Cellar/opencv/4.1.0_2/include/opencv4/opencv"
+  "/usr/local/Cellar/opencv/4.1.0_2/include/opencv4"
+  "/usr/local/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
