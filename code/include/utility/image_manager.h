32aabcc3783bfcc6e5b4629adedf2726c166b253
Martin <martin.carrasco@utec.edu.pe>
2019-06-28 14:18:38 -0500

    Refactored into more modular approach, added zero padding for not power of 2 images

--- code/include/utility/image_manager.h
+++ code/include/utility/image_manager.h
@@ -2,25 +2,70 @@
 #define __IMAGE_MANAGER_H__
 
 #include "algorithms/2dfft.h"
-
-#include <cmath>
-#include <fstream>
 #include <sstream>
 #include <vector>
 
-float dist_e(int cx, int cy, int px, int py);
+class ImageFilter {
+ public:
+  virtual std::vector<std::vector<cn>> make_filter(
+      std::vector<std::vector<cn>> matrix, int width, int height, int d0) = 0;
+};
 
-bool cmp_complex(cn a, cn b);
+class ImageMedianFilter : public ImageFilter {
+ public:
+  std::vector<std::vector<cn>> make_filter(std::vector<std::vector<cn>> matrix,
+                                           int width, int height, int d0);
+};
 
 std::vector<std::vector<cn>> low_pass_filter(int width, int height, int d0);
-
-std::vector<std::vector<cn>> median_filter(std::vector<std::vector<cn>> vec,
+class ImageLowFilter : public ImageFilter {
+ public:
+  std::vector<std::vector<cn>> make_filter(std::vector<std::vector<cn>> matrix,
                                            int width, int height, int d0);
+};
 
-void save_img(std::vector<std::vector<cn>> vec);
+class ImageUtils {
+ public:
+  static bool cmp_complex(cn a, cn b) { return real(a) < real(b); }
+  static std::vector<std::vector<cn>> pad0_complex(
+      std::vector<std::vector<cn>>& matrix, int width, int height) {
+    std::vector<std::vector<cn>> rows(width * 2,
+                                      std::vector<cn>(height * 2, 0));
+    for (int x = 0; x < width; x++) {
+      for (int y = 0; y < height; y++) {
+        rows[x][y] = matrix[x][y];
+      }
+    }
+    return rows;
+  }
+  static void center_matrix(std::vector<std::vector<cn>>& matrix) {
+    int width = matrix.size();
+    int height = matrix[0].size();
+    for (int x = 0; x < width; x++) {
+      for (int y = 0; y < height; y++) {
+        matrix[x][y] = matrix[x][y] * pow(-1, x + y);
+      }
+    }
+  }
+  static float dist_euclid(int cx, int cy, int px, int py) {
+    return sqrt(pow(cx - px, 2) + pow(cy - py, 2));
+  }
+};
 
-std::vector<std::vector<cn>> load_img(std::string filename);
+class ImageTransform {
+ private:
+  std::vector<std::vector<cn>> complex_matrix;
 
-std::vector<std::vector<cn>> img_transform(std::vector<std::vector<cn>> matrix);
+ public:
+  ImageTransform(void);
+  ImageTransform(std::vector<std::vector<cn>> matrix);
+  ImageTransform(std::vector<std::vector<double>> matrix);
 
+  void apply_transform(ImageFilter* filter);
+
+  void set_matrix(std::vector<std::vector<cn>> matrix);
+  void set_matrix(std::vector<std::vector<double>> matrix);
+
+  std::vector<std::vector<cn>> get_matrix(void);
+};
 #endif
--- code/src/main.cpp
+++ code/src/main.cpp
@@ -4,13 +4,21 @@
 
 using namespace std;
 
-int main(void)
-{
-    generate_csv();
-    vector< vector<cn> > img_matrix = load_img("res/image/output/output.txt");
-    img_matrix = img_transform(img_matrix);
-    save_img(img_matrix);
-    display_csv();
-    
-    return 0;
+int main(void) {
+  vector<vector<cn>> img_matrix = make_2d_array();
+
+  // display_csv(img_matrix);
+
+  ImageTransform transformer(img_matrix);
+
+  ImageLowFilter* filter = new ImageLowFilter();
+
+  transformer.apply_transform(filter);
+
+  vector<vector<cn>> trans_matrix = transformer.get_matrix();
+
+  // save_img(img_matrix);
+  display_csv(trans_matrix);
+
+  return 0;
 }
