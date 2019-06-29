6cf49e4058c4f206a5e1ebc4451cc691f8ef3b23
dgcnz <canezdiego@gmail.com>
2019-06-29 12:02:03 -0500

    asdasd

--- code/src/main_audio.cpp
+++ code/src/main_audio.cpp
@@ -1,12 +1,8 @@
-#include "applications/fourier_audio.h"
 #include <vector>
-#include <iostream>
 
 using namespace std;
 
 int main(void) {
-    cout << "Playing audio sample\n";
-    test_audio();
-    cout << "Finished.\n";
-    return 0;
+
+  return 0;
 }
--- code/src/main_image.cpp
+++ code/src/main_image.cpp
@@ -1,9 +1,18 @@
+#include "utility/image_manager.h"
+#include "utility/opencv_manager.h"
 #include <vector>
 
 using namespace std;
 
-int main(void)
-{
+int main(void) {
+  vector<vector<cn>> img_matrix = make_2d_array();
 
-    return 0;
+  ImageTransform transformer(img_matrix);
+
+  transformer.apply_lp(0.0);
+
+  // save_img(img_matrix);
+  display_csv(img_matrix);
+
+  return 0;
 }
